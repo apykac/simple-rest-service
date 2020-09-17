3f20258c913b34a10f129426a9990f3df20a1797
Sergey Semushev <sergey.semushev@gazprombank.ru>
2020-09-17 11:49:20 +0300

    fix

--- src/main/java/ru/home/controller/CountryController.java
+++ src/main/java/ru/home/controller/CountryController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.home.dto.Country;
@@ -25,8 +26,8 @@ public class CountryController {
         return countryRepository.getCountry(countryName);
     }
 
-    @PostMapping("")
-    public Country addCountry(Country country) {
+    @PostMapping("/add")
+    public Country addCountry(@RequestBody Country country) {
         return countryRepository.addCountry(country);
     }
 
--- src/main/java/ru/home/controller/PersonController.java
+++ src/main/java/ru/home/controller/PersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.home.dto.Person;
@@ -25,8 +26,8 @@ public class PersonController {
         return personRepository.findPerson(personName);
     }
 
-    @PostMapping("")
-    public Person addCountry(Person person) {
+    @PostMapping("/add")
+    public Person addCountry(@RequestBody Person person) {
         return personRepository.addPerson(person);
     }
 
